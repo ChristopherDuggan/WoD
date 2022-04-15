05e15548db805b5ba73562f0e5e6ed86754f960a
Christopher Duggan <christopherduggan@gmail.com>
2022-04-15 17:03:27 -0400

    adds some dramatic feats

--- rules.js
+++ rules.js
@@ -200,7 +200,8 @@ ACTION TURNS
         act highest to lowest. In the case of a tie, the character wtih the
         highest 'initiative rating' (dex + wit). If that's tied, favor the
         character with the highest wit. If that's tied, favor the character with
-        the highest dex. 
+        the highest dex. (Optionally rolled once at the start of a sequence or
+        once per turn)
 
         oWoD: Characters declare their actions lowest init to highest init, then
         actions resolve highest to lowest
@@ -221,4 +222,157 @@ AUTOMATIC FEATS - no roll needed in normal circumstances
     normal speech, writing, or texting
     grabbing tools or weapons
 
+                                    MOVEMENT
+
 /*********************************MOVEMENT RATES*******************************\
+*                                                                              *
+*       Move        Distance Per Turn                                          *
+*       Walk        7 yards                                                    *
+*       Jog         12 + Dexterity Yards                                       *
+*       Run         20 + (3 * Dexterity) Yards                                 *
+*       Swim        8 + dex (unskilled) or 12 + dex (skilled) yards            *
+*       Fly         10 to 20 yards                                             *
+*       Climb       10 feet(3.3 yards) per success normally; 15-20 under ideal *
+*                   conditions; 5 feet per success under poor conditions       *
+*                                                                              *
+\******************************************************************************/
+
+/*
+A character can move half of their normal running speed in a single turn and
+still act without taking a penalty. Beyond that, they have to reduce their die
+pool for that second action by one for every yard moved.
+
+A character can move at full speed for roughly 10 turns for every point of
+stamina they possess.
+
+**health penalty** Characters who have reached the injured Health Level or lower
+move at half their normal rate.
+
+High intensity movement (jumping, swimming, sprinting) can cut the normal
+movement duration by half. It can be further cut due to extreme circumstances
+(swiming through heavy waves, dodging gunfire, outrunning a bear). To push
+beyond limits, the character may roll Stamina or Willpower (whichever is
+greater). A botch on this roll and the character trips, their body gives out, or
+some other movement stopping effect. 
+
+                                 DRAMATIC FEATS
+/******************************************************************************\
+* ACTION        TRAITS              DIFF    DESCRIPTION   
+
+                                 Physical Feats
+* Chase         dex + athletics     6       resisted * extended
+  Climbing      dex + athletics     v *1    extended; 1 success = 5 ft
+  Demolition    str + athletics     v       extended; 1 roll per strike
+  Flow-Arts     dex + athletics     5-9     standard; 1 roll per performance
+  Foraging      per + survival      7-9     extended; 1 roll = 2-5 hours
+  Hunting       per + survival*2    7-9     extended; 1 roll = 1-3 hours
+  Jumping       str                 3       standard; up 2'/success
+                                            across 4'/success
+  Lifting       str or will         v       see Feats of Strength chart
+  Resisting     will or sta         +3 (v)  standard or resisted; see torture
+  Sneaking      dex + stealth       *3      standard; see spotting sneak
+  Swimming      sta + athletics     v       extended; 1 success = 10 minutes
+                or swimming                 for speed see movement rates
+
+                                Art and Science
+ Computer       int + computers     v       standard or extended
+ Craftwork      dex + crafts *4     v       extended; 1 roll = a day, week,
+                                            month, depending on task
+ Design         int + crafts *4     v       extended
+ Evaluation     per + art or crafts v       extended
+ Gamesmanship   per + subterfuge*5  v       resisted
+ Hacking        int or wit +        *6      extended and resisted
+                computer
+ Invention      int + craft or      v       extended
+                technology  
+ Jury-Rigging   wit + tech          v       standard
+ Performance    man + art*7         v       standard
+ Programming    int + comptuer      *6      extended
+ Repair         int + tech          v       extended
+ Research       int + research*8    *9      extended
+ Surfing  (web) per + computer      *9      extended
+ Understanding  int + enigmas       v       standard
+                or esoterica
+                         Social Occasions And Intrigue
+ Cryptography   int + knowledge*10  8       extended
+ Deception*11   man + subterfuge    *12     resisted
+ Eloquence*11   cha + expression    7       standard
+ Facedowwn*11   cha + intimidation  8       resisted, target's willpower
+ Fake-Out       man + streetwise    *12     standard
+ Gossip*11      man + subterfuge    7       standard
+ Impressing*11  cha + etiquette     v       standard
+ Interpreting   per + empathy       v       standard
+ Interogate*11  man + intimidation  6       resisted, target's willpower
+ Poisoning      dex + streetwise    *12     standard
+ Seduction*11   man + subterfuge or *12     resisted, target's willpower
+                seduction
+ Spotting Sneak per + stealth       8       resisted; see Sneaking
+ Spotting       per + awareness     v       standard
+ Supernatural
+ Torture*11     man + torture       6       resisted; see Resisting
+
+                                 Transportation
+ Driving        dex + drive         *14     standard
+ Evasion        dex + transport     *14     resisted*15
+                method
+ Flight         dex + flying or     *14     standard
+                jetpack
+ Piloting       dex + wit or        *14     standard
+                pilot
+ Pursuit        dex + transport     *14     resisted*16; see Evasion
+                method
+ Riding         dex + riding        *14     standard                
+ Sailing        wit + crafts        *14     standard                
+                (seamanship)
+
+                                     Notes
+#1  Typically 6 but may go up or down depending on circumstances
+#2  Int can replace Per, to reflect cunning
+#3  Watcher's per + awareness; could also be set by conditions - higher for light
+    or open space, lower for darkness or cover
+#4  Craft reflects the appropriate Trait; it could also be Art or Knowledge
+#5  Or a knowledge trait related to the game. Could also be man + intimidation
+    (to bluff); per + intuition (to read opponent); or int + enigmas (to work
+    out puzzle).
+#6  Depends upon system being hacked; for details se extended computer rules in 
+    The Book of Secrets
+#7  Man could be replaced by dex, sta, cha, or app, depending on the type of
+    performance. Expression, intimidation, or seduction could be used to
+    compensate for lack of skill
+#8  Research can be replaced by streetwise (for dirt), etiquette (high society),
+    computer (for computer searches), or other appropriate knowledge traits
+    (esoterica, government, investigation, etc)
+#9  Depends on how accessible the information is: 4 = widely available, 6 =
+    normally accessible, 8 = hard to find, 9 + threshold = secret, classified,
+    or assumed lost
+#10 Knowledge reflects the trait necessary to encode or decode the
+    information. Typically code traits include Esoterica (appropriate
+    specialty), linguistics (appropriate language), science (appropriate
+    discipline), or other fields. To crack a code without the appropriate
+    information, you might use computer(to access databases) at a +3 diff and
+    twice the normal successes needed; cracking the code when you do have the
+    information speeds the process considerably. Creating or cracking complex
+    codes is an extended activity, with each roll reflecting one day, week, or
+    month, depending on the code's complexity. Many modern ciphers cannot be
+    decoded without computers, and take days or weeks to unravel even then.
+#11 Any social trait works; Cha reflects force of personality, man is obvious,
+    and appearance lets you use beauty to your advantage.
+#12 Difficulty is observer's wit + subterfuge.
+#13 Torturer makes one roll per turn. Each scene of mental torture costs the
+    victim one point of temporary willpower.
+    Each scene of physical torture cost the victim one health level of bashing
+    or lethal damage depending on method; intense torment inflicts two health
+    levels per scene). Intense or lethal torture reduces tormenter's difficulty
+    by -2. Botched roll gets false information, breaks target's mind, or
+    possibly kills target.
+#14 Difficulties depend upon travel conditions, from 3 or 4 in calm conditions
+    to 9 + threshold under hazardous conditions.
+#15 Target may use dex + stealth on foot(wit + stealth to duck behind
+    cover). Pursuer's roll is resisted with per + subterfuge, and the pursuers must
+    win roll in order to find target.
+#16 Pursuer tries to equal or exceed target's successes; target tries to evade
+    pursuer. Target begins with a head start between one and four successes. (One
+    success = 2 yards on foot, 10 yards in cars, 50 yards in large
+    vehicles). Each success by pursuer eliminates one sucess from the target. If
+    pursuer matches or exceeds target's successes, pursuer catches up; if target
+    more than doubles pursuer's successes, target escapes.
